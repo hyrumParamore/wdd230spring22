0072a79c9fb48e8a30c83dbe44039583eda8c5e9
hyrumParamore <hyrumparamore@gmail.com>
2022-07-15 14:55:48 -0600

    yup

--- 1portfolio/scripts/index.js
+++ 1portfolio/scripts/index.js
@@ -46,7 +46,7 @@ localStorage.setItem('last_visited', now_date);
 
 
 
-function openCity(evt, cityName) {
+function openCity(evt, pageName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
@@ -56,7 +56,7 @@ function openCity(evt, cityName) {
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    document.getElementById(cityName).style.display = "block";
+    document.getElementById(pageName).style.display = "block";
     evt.currentTarget.className += " active";
   }
   
