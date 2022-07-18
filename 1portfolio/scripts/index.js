10e116f0f9245ae69bbcddd9c9d4e0503ae06c81
hyrumParamore <hyrumparamore@gmail.com>
2022-07-18 17:04:53 -0600

    yodates

--- 1portfolio/scripts/index.js
+++ 1portfolio/scripts/index.js
@@ -118,3 +118,18 @@ function Snackbar() {
     // After 3 seconds, remove the show class from DIV
     setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
   }
+
+
+
+// **********************
+//      Scroll Bar
+// **********************
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
