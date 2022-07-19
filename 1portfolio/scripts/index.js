9cd49d2742e3c6cc512dc621c8e1352805ad84e9
hyrumParamore <hyrumparamore@gmail.com>
2022-07-18 18:02:58 -0600

    yupdates

--- 1portfolio/scripts/index.js
+++ 1portfolio/scripts/index.js
@@ -1,39 +1,39 @@
-// // *****************
-// //   LOCAL STORAGE
-// // *****************
+// // // *****************
+// // //   LOCAL STORAGE
+// // // *****************
 
-// local storage
-localStorage.setItem('msg1', 'Hello Permanent');
-sessionStorage.setItem('msg2', 'Hi Temporary');
-console.log(localStorage.getItem('msg1'));
+// // local storage
+// localStorage.setItem('msg1', 'Hello Permanent');
+// sessionStorage.setItem('msg2', 'Hi Temporary');
+// console.log(localStorage.getItem('msg1'));
 
-// Days since last visit
-let visit_heading = document.querySelector('.visited');
+// // Days since last visit
+// let visit_heading = document.querySelector('.visited');
 
-// Gets the date
-let now_date = new Date();
+// // Gets the date
+// let now_date = new Date();
 
-// Converts the string in the local storage back to the date. NEED
-// TO DO IT EVERYTIME YOU NEED TO PULL DATA OUT OF THE STORAGE!!!
+// // Converts the string in the local storage back to the date. NEED
+// // TO DO IT EVERYTIME YOU NEED TO PULL DATA OUT OF THE STORAGE!!!
 
 
-// This is this weekends homework to calculate the difference in days they visited
-let stored_date = localStorage.getItem('last_visited');
-let last_date = new Date(stored_date);
+// // This is this weekends homework to calculate the difference in days they visited
+// let stored_date = localStorage.getItem('last_visited');
+// let last_date = new Date(stored_date);
 
-if(!stored_date) {
-    visit_heading.textContent = "Welcome!";
-}
+// if(!stored_date) {
+//     visit_heading.textContent = "Welcome!";
+// }
 
-else {
-    // calc the diff to the two dates
-    let diff_time = now_date.getTime() - last_date.getTime();
-    let diff_day = Math.round(diff_time/(1000 * 60 * 60 * 24));
-    visit_heading.textContent = diff_day + " days since you last visited!";
-}
+// else {
+//     // calc the diff to the two dates
+//     let diff_time = now_date.getTime() - last_date.getTime();
+//     let diff_day = Math.round(diff_time/(1000 * 60 * 60 * 24));
+//     visit_heading.textContent = diff_day + " days since you last visited!";
+// }
 
-// Converts the date into a string in the local storage
-localStorage.setItem('last_visited', now_date);
+// // Converts the date into a string in the local storage
+// localStorage.setItem('last_visited', now_date);
 
 
 
@@ -45,22 +45,23 @@ localStorage.setItem('last_visited', now_date);
 
 
 
-function openCity(evt, cityName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
-  }
+// function openCity(evt, cityName) {
+//     var i, tabcontent, tablinks;
+//     tabcontent = document.getElementsByClassName("tabcontent");
+//     for (i = 0; i < tabcontent.length; i++) {
+//       tabcontent[i].style.display = "none";
+//     }
+//     tablinks = document.getElementsByClassName("tablinks");
+//     for (i = 0; i < tablinks.length; i++) {
+//       tablinks[i].className = tablinks[i].className.replace(" active", "");
+//     }
+//     document.getElementById(cityName).style.display = "block";
+//     evt.currentTarget.className += " active";
+//   }
   
-  // Get the element with id="defaultOpen" and click on it
-  document.getElementById("defaultOpen").click();
+//   // Get the element with id="defaultOpen" and click on it
+// document.getElementById("defaultOpen").click();
+
 
 
 
@@ -118,7 +119,6 @@ function Snackbar() {
     // After 3 seconds, remove the show class from DIV
     setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
   }
-
 
 
 // **********************
--- practice/index.js
+++ practice/index.js
@@ -14,3 +14,35 @@ function openCity(evt, cityName) {
   
   // Get the element with id="defaultOpen" and click on it
   document.getElementById("defaultOpen").click();
+
+
+
+
+
+
+  // SNACKBAR POP-UP
+function Snackbar() {
+  // Get the snackbar DIV
+  var x = document.getElementById("snackbar");
+
+  // Add the "show" class to DIV
+  x.className = "show";
+
+  // After 3 seconds, remove the show class from DIV
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
+
+
+
+// **********************
+//      Scroll Bar
+// **********************
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+var scrolled = (winScroll / height) * 100;
+document.getElementById("myBar").style.width = scrolled + "%";
+}
