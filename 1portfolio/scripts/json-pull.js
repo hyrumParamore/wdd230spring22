ee546c5bc39e1a006421273e1ef9b9178ed69202
hyrumParamore <hyrumparamore@gmail.com>
2022-07-19 14:10:36 -0600

    asd

--- 1portfolio/scripts/json-pull.js
+++ 1portfolio/scripts/json-pull.js
@@ -1,59 +1,13 @@
+
+
+// ****************
+// Projects Page, Side Projects Tab Cards.
+// This information is brought in through a JSON file.
+
 const requestURL = 'https://hyrumparamore.github.io/wdd230spring22/1portfolio/json-files/json.json';
-// const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
 
-// .THEN function
-// fetch(requestURL)
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (jsonObject) {
-//     console.table(jsonObject);  // temporary checking for valid response and data parsing
-//   });
-
-
-
-
-// function showCards() {
-//   let section2 = document.querySelectorAll('tr');
-//   section2.forEach((tr) =>
-//     tr.remove());
-//   getBusinesses(requestURL);
-// }
 
 getBoxes(requestURL)
-
-
-
-
-
-// function showTable() {
-//   let section = document.querySelectorAll('.cards section');
-//   section.forEach((cards) =>
-//     cards.remove());
-//   getTable(requestURL);
-// }
-
-
-
-// let button2 = document.querySelector('#button2');
-
-
-// let button1 = document.querySelector('#button1');
-
-
-
-// ASYNC function
-// async function getTable(requestURL) {
-//   const response = await fetch(requestURL);
-//   if (response.ok) {
-//     const jsonObject = await response.json();
-//     console.log(jsonObject);
-
-//     const business = jsonObject['businesses'];
-//     // business.forEach(displayBusinesses);
-//     business.forEach(displayTable);
-//   }
-// }
 
 async function getBoxes(requestURL) {
   const response = await fetch(requestURL);
@@ -63,66 +17,35 @@ async function getBoxes(requestURL) {
 
     const box = jsonObject['boxes'];
     box.forEach(displayBoxes);
-    // business.forEach(displayTable);
   }
 }
-
-// getTable(requestURL);
-// getBusinesses(requestURL);
 
 
 function displayBoxes(boxes)
 {
     
-    let box = document.createElement('div');
+    var box = document.createElement('div');
+    box.className = "side-project-boxes";
+
     let h2 = document.createElement('h2');
     let ex = document.createElement('p');
     let p = document.createElement('p');
-    // let pho = document.createElement('p');
-    // let pob = document.createElement('p');
     let img = document.createElement('img');
+    img.className = "images"
 
     h2.textContent = boxes.title; 
     ex.textContent = boxes.extra;   
-    p.textContent = boxes.text;
-    // pho.textContent = boxes.phone;
-    // pob.textContent = `${boxes.birthplace}`;                             
+    p.textContent = boxes.text;                           
     img.setAttribute('src', boxes.imageurl);                                     
-    img.setAttribute('alt', `${boxes.title}`) // This :)
+    img.setAttribute('alt', `${boxes.title}`)
 
 
     box.appendChild(img);
     box.appendChild(h2);
     box.appendChild(ex);
     box.appendChild(p);
-    // box.appendChild(web);
-    // card.appendChild(pob);
-    
-    document.querySelector('.flex-boxes').appendChild(box);
+
+    document.querySelector('.this').appendChild(box);
 
 }
 
-
-// function displayTable(business) 
-// {
-//   let list_row = document.createElement('tr');
-//   let td_name = document.createElement('td');
-//   td_name.textContent = business.name;
-
-//   let td_address = document.createElement('td');
-//   td_address.textContent = business.address;
-
-//   let td_phone = document.createElement('td');
-//   td_phone.textContent = business.phone;
-
-//   let td_website = document.createElement('td');
-//   td_website.textContent = business.website;
-
-
-//   list_row.appendChild(td_name);
-//   list_row.appendChild(td_address);
-//   list_row.appendChild(td_phone);
-//   list_row.appendChild(td_website);
-//   document.querySelector('table').appendChild(list_row);
-
-// }
